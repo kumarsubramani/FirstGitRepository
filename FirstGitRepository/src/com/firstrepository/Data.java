300e309cffd95f1a227e4f61869d001657799721
Kumar Subramani <kumarsubramani@outlook.com>
2014-06-29 00:48:18 +0530

    inital checkin of project FirstGitRepository

---
+++ FirstGitRepository/src/com/firstrepository/Data.java
@@ -0,0 +1,8 @@
+package com.firstrepository;
+
+public class Data {
+
+	public void display() {
+		System.out.println("Welcome to Git Repository");
+	}
+}
---
+++ FirstGitRepository/src/com/firstrepository/Main.java
@@ -0,0 +1,8 @@
+package com.firstrepository;
+
+public class Main {
+	public static void main(String args[]) {
+		Data data = new Data();
+		data.display();
+	}
+}
